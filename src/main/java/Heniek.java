8a0b1fdcbe25c00dd1204308f1ea015f771209c7
bedziox <daniel.bednarek2001@gmail.com>
2023-02-04 17:14:32 +0100

    logging bot and adding secured token

--- src/main/java/Heniek.java
+++ src/main/java/Heniek.java
@@ -1,5 +1,19 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+
 public class Heniek {
-    public static void main(String[] args) {
-        System.out.println("TEST");
+    public static void main(String[] args) throws LoginException {
+        Dotenv dotenv = Dotenv.configure().load();
+        JDABuilder jdaBuilder = JDABuilder.createDefault(dotenv.get("BOT_TOKEN"));
+        try{
+            jdaBuilder.build();
+        }
+        catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
